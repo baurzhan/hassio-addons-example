95ec84591ba3b00bbdf139f49edce779c8582de0
Baurzhan Taishinov <baurzhan@gmail.com>
2019-08-31 02:29:12 +0600

    add image name

--- l2tp-client/build.sh
+++ l2tp-client/build.sh
@@ -1,1 +1,2 @@
-docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t l2tp-client -r https://github.com/baurzhan/hassio-addons-example.git -b master
+#docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t l2tp-client -r https://github.com/baurzhan/hassio-addons-example.git -b master
+docker run --rm --privileged -v ~/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock -v ~/Work/CH/hassio-addons-example/l2tp-client:/data homeassistant/amd64-builder --amd64 -t /data
