26db6644f7b41716bfed0e3b8437329b6986b25c
Baurzhan Taishinov <baurzhan@gmail.com>
2019-09-03 00:32:29 +0600

    Change user-id file location

--- l2tp-client/run.sh
+++ l2tp-client/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-FILE=/share/user-id
+FILE=/data/user-id
 cat $FILE
 if [ ! -f $FILE ]; then
 	(/bin/cat /dev/urandom || true) | (/usr/bin/tr -dc "a-z0-9" || true) | (/usr/bin/fold -w 12 || true) | (/usr/bin/head -n 1 || true) | (tee $FILE || true)
