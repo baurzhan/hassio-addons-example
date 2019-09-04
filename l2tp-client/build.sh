1adca93984922c56053b90ab3cb3bc609889dd98
Baurzhan Taishinov <baurzhan@gmail.com>
2019-09-04 13:06:07 +0600

    Remove client sources

--- l2tp-client/build.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm --privileged \
-	-v ~/.docker:/root/.docker \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	homeassistant/amd64-builder --all -t l2tp-client \
-	-r https://github.com/baurzhan/hassio-addons-example.git -b master
-#docker run --rm --privileged -v ~/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock -v ~/Work/CH/hassio-addons-example/l2tp-client:/data homeassistant/amd64-builder --all -t /data
--- l2tp-client/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-FILE=/data/user-id
-if [ ! -f $FILE ]; then
-	(/bin/cat /dev/urandom || true) | (/usr/bin/tr -dc "a-z0-9" || true) | (/usr/bin/fold -w 12 || true) | (/usr/bin/head -n 1 || true) | (tee $FILE || true)
-fi
-/usr/bin/supervisord -c /supervisord.conf
